c170ced002660afa69d879bef90b4b504907aaa8
Artur <artur-fedyuk@mail.ru>
2018-03-02 18:17:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  num = 0; 
+  exp= 5;
+    while(num > exp)
+    {
+        num += Math.floor(num / exp);
+        exp *= 5;
+    }
+return num;
 }
